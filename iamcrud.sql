6b53ec5c70909e305a3e13b8fb7d663a3e02125f
ShinAquila <augusto08rivera12@gmail.com>
2024-09-16 17:22:21 +0800

    added sql file

---
+++ iamcrud.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2024 at 11:21 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iamcrud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fruits`
+--
+
+CREATE TABLE `fruits` (
+  `fruit_id` int(11) NOT NULL,
+  `fruit_name` varchar(255) NOT NULL,
+  `fruit_qty` int(11) NOT NULL,
+  `fruit_created` datetime NOT NULL,
+  `fruit_updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fruits`
+--
+ALTER TABLE `fruits`
+  ADD PRIMARY KEY (`fruit_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fruits`
+--
+ALTER TABLE `fruits`
+  MODIFY `fruit_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
