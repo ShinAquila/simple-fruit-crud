e16d0419cde08bd97b76a1c6706cfb0e7b09b4fd
ShinAquila <augusto08rivera12@gmail.com>
2024-09-19 17:22:03 +0800

    functioning create fruit

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ require '_functions.php';
                         </div>
                     </div>                
                     <div class="modal-footer">
-                        <button type="button" name="createFruit" id="createFruit" class="btn btn-primary">Create</button> 
+                        <button type="submit" name="createFruit" id="createFruit" class="btn btn-primary">Create</button> 
                         
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </div>    
